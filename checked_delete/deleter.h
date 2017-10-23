63c0b01f7f4af60fa85e6bf771c2150508681fcf
Hanyu Zhang <hanyu.zhang@beacon.io>
2017-10-23 15:37:24 -0400

    add comment

--- checked_delete/deleter.h
+++ checked_delete/deleter.h
@@ -8,7 +8,12 @@ public:
     GoodObj();
     ~GoodObj();
 };
-
+// non-specialized template must be defined in header file or another header
+// file to wrap this implementation. It cannot be decleared in header file and
+// implemented in cpp file. Itmust be visible to a translation unit that uses it.
+//
+// The compiler must be able to see the implementation in order to generate
+// code for all specializations
 template <typename T>
 void delete_obj(T* p){
     typedef char type_must_be_complete[sizeof(T) ? 1 : -1];
