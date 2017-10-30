59cdcfee940e521e0516d177f6a9c94e39c15dfb
Hanyu Zhang <hanyu.zhang@beacon.io>
2017-10-30 16:20:10 -0400

    add commit for checked_delete

--- checked_delete/main.cpp
+++ checked_delete/main.cpp
@@ -1,5 +1,8 @@
 #include "deleter.h"
-
+/* this idiom is used to delete object porperly
+ * if one object is partially defined, then throw an error
+ * otherwise delete that object
+ * */
 int main(){
 //    Object *p = new Object();
     GoodObj *p = new GoodObj();
